f6f60bc71f1be4b891e56e311719769b540319e7
anmol07goyal <anmol77goyal@gmail.com>
2023-03-03 01:02:19 -0500

    almost final update
    added sounds, focus on player, fog particle effects

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -1,10 +1,11 @@
+using System;
 using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
     #region GameObjects
 
-    public Transform eyeGO;
+    // public Transform eyeGO;
     [SerializeField] private Transform currentCamera;
 
     #endregion
@@ -25,23 +26,43 @@ public class LookAtPlayer : MonoBehaviour
         // startRotation = eyeGO.localRotation;
     }
 
+    public Vector3 angle;
+    
+    private void Update()
+    {
+        // transform.LookAt(currentCamera.transform.localPosition + angle);
+    }
+
+    public Vector3 distance;
+
     private void LateUpdate()
     {
-        if (lookAtDistance > Vector3.Distance(eyeGO.position, currentCamera.position))
+        if (lookAtDistance > Vector3.Distance(transform.position, currentCamera.position))
         {
+            distance = currentCamera.position - transform.position;
             inProximity = true;
             currentRotation = Quaternion.Slerp(
-                currentRotation, 
-                Quaternion.LookRotation(currentCamera.position - eyeGO.position), 
+                currentRotation,
+                Quaternion.LookRotation(distance),
                 Time.deltaTime * lookAtSpeed);
-            
-            eyeGO.localRotation = currentRotation;
+    
+            transform.localRotation = Quaternion.Euler(currentRotation.eulerAngles + angle);
         }
         else
         {
             // currentRotation = Quaternion.Slerp(currentRotation, eyeGO.rotation, Time.deltaTime * lookAtSpeed);
             inProximity = false;
         }
-
+    
     }
+    
+    // public float rotationModifier, speed;
+    // private void FixedUpdate()
+    // {
+    //     Vector3 vectorToTarget = currentCamera.transform.position - transform.position;
+    //     float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
+    //     Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+    //     transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+    //
+    // }
 }
